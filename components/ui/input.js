f9d84331f521e97ec90261651f57ec341fbb6325
bswaimttb <ben@ttbtx.com>
2025-06-07 09:13:21 -0500

    Create input.js

---
+++ components/ui/input.js
@@ -0,0 +1,8 @@
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      className="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
+    />
+  );
+}
